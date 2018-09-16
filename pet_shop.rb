1073097ad5c813268ad107436026fb6d9b569e32
Jackie Farr <jacquelynfarr@hotmail.com>
2018-09-16 16:26:56 +0100

    homework tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -22,16 +22,59 @@ end
 def stock_count(place)
   @pet_shop[:pets].count
 end
-#
-def pets_by_breed(place, pet_breed)
-  result = []
-  for breed in @pet_shop[:pets][:breed]
-    if breed @pet_shop[:pets][:breed] == pet_breed
-    result << pet_breed
-    end
-  end
-  return result
-end
-#
+# # this didn't work:
+# def pets_by_breed(place, pet_breed)
+#   for pets in @pet_shop[:pets]
+#     if pets[:breed] == pet_breed
+#     end
+#     return pets
 #   end
 # end
+
+def pets_by_breed(pet_shop, breed)
+   result = []
+     for animal in pet_shop[:pets]
+       if animal[:breed] == breed
+         result << animal
+       end
+     end
+     return result
+end
+
+# #
+def find_pet_by_name(shops, name)
+  for pet in shops[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+return nil
+end
+
+def remove_pet_by_name(shops, name)
+  for pet in shops[:pets]
+    if pet[:name] == name
+      shops[:pets].delete(pet)
+    end
+  end
+end
+
+def add_pet_to_stock(shops, new_pet)
+  shops[:pets].push(new_pet)
+end
+
+def customer_cash(customers)
+  return customers[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  return customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push(pet)
+end
